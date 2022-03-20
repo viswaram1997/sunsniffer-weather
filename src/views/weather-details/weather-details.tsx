933e88347373e48c1586b20f9e58d046edddac7a
viswaram13 <82084568+viswaram13@users.noreply.github.com>
2022-03-20 21:13:18 +0530

    Add base setup
    - create  project using create react app
    - add sass
    - add placeholder components

---
+++ src/views/weather-details/weather-details.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { ComponentPropsType } from "./types"
+
+const WeatherDetails: React.FC<ComponentPropsType> = () => {
+    return (
+        <div>
+           Details Page
+        </div>
+    )
+}
+
+export default WeatherDetails
